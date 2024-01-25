5e3c78d3b0886b0556ad09f617b9948acf43d072
ide-editor <114766310+ide-editor@users.noreply.github.com>
2024-01-25 10:26:31 +0200

    Update IDE.js
    fix colors picker selection

--- IDE.js
+++ IDE.js
@@ -1,3 +1,6 @@
+document.getElementById("Colors_Picker_type").src="ColorPicker.html?mode="+document.getElementById("setColorsPicker").value;
 setTimeout(function(){
   setColorsPicker();
 },1);
+document.getElementById("Colors_Picker_type").src="ColorPicker.html?mode="+document.getElementById("setColorsPicker").value;
+setColorsPicker();
