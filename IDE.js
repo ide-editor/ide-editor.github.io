50fbacef6b5197f61c0c838036c462c96e93074f
ide-editor <114766310+ide-editor@users.noreply.github.com>
2024-01-25 10:28:12 +0200

    Delete IDE.js

--- IDE.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("Colors_Picker_type").src="ColorPicker.html?mode="+document.getElementById("setColorsPicker").value;
-setTimeout(function(){
-  setColorsPicker();
-},1);
-document.getElementById("Colors_Picker_type").src="ColorPicker.html?mode="+document.getElementById("setColorsPicker").value;
-setColorsPicker();
